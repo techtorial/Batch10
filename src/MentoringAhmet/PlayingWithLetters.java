6070771c8b34ede24f5da695abada2b4addad236
techtorial <info@techtorialacademy.com>
2021-09-14 21:09:02 -0500

    mentoring,reverse and playing with letters

---
+++ src/MentoringAhmet/PlayingWithLetters.java
@@ -0,0 +1,37 @@
+package MentoringAhmet;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class PlayingWithLetters {
+    //  ask user to enter one string value with three word
+//1- Print first letter of each word
+//2- Print last letter of each word
+// 3-Print the sum of last letter of each word's index number
+// 4-Print the sum of first letter of each word's index number
+//    Example: "Java String Test"
+//    Output:
+//    JST
+//    agt
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("please enter whatever you want ");
+        String words = scan.nextLine();
+        String firstLetters = "";
+        String lastLetters="";
+        firstLetters += words.charAt(0);
+        for (int i = 0; i < words.length(); i++) {
+            if (words.charAt(i) == ' ') {
+                firstLetters += words.charAt(i + 1);
+                lastLetters+=words.charAt(i-1);
+            }
+        }
+        lastLetters+=words.charAt(words.length()-1);
+        System.out.println(firstLetters);
+        System.out.println(lastLetters);
+    }
+
+}
+
+
---
+++ src/MentoringAhmet/ReverseStringOrNumber.java
@@ -0,0 +1,29 @@
+package MentoringAhmet;
+
+import java.util.Scanner;
+
+public class ReverseStringOrNumber {
+
+    //Ask user to input some words and sout the reverse of the words.
+    //String words=Hello My name is Ahmet
+    public static void main(String[] args) {
+        //step1:Read the question carefully and understand what it asks for
+        //step2:Start making my logic
+        //step3: I need to ask user to input some words
+        //string="Hello My name is Ahmet"
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Please enter the words");
+        String words = scan.nextLine();
+        //step4: i need to reach out every single letter HOw?
+        String reverse="";
+        for(int i = words.length()-1 ;i>=0;i--){
+           reverse+=words.charAt(i);
+        }
+        System.out.print(reverse);
+        //step5: I need to start from last not from beginning How ?
+        //step6: I need to show them on console I think i know that :)
+
+
+    }
+
+}
